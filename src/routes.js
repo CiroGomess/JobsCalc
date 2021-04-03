3688119d9f84fce94fe0c9be4e2c6da8b4f3639e
CiroGomess <ciro.ggj@gmail.com>
2021-04-03 14:36:07 -0300

    Passando os dados das rotas para as views Profile✅

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,24 @@ const routes = express.Router()
 
 const views = __dirname + '/views'
 
+const profile = {
+    name: 'Ciro Gomes',
+    avatar: 'https://avatars.githubusercontent.com/u/43193194?v=4',
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4
+}
+
+
 // Rotas 
-routes.get('/', (req, res) =>  res.render(`${views}/index`))
-routes.get('/job', (req, res) =>  res.render(`${views}/job`))
-routes.get('/job/edit', (req, res) =>  res.render(`${views}/job`))
-routes.get('/profile', (req, res) => res.render(`${views}/profile`))
+routes.get('/', (req, res) => res.render(`${views}/index`))
+
+routes.get('/job', (req, res) => res.render(`${views}/job`))
+
+routes.get('/job/edit', (req, res) => res.render(`${views}/job`))
+
+routes.get('/profile', (req, res) => res.render(`${views}/profile`, { profile: profile }))
 
 
 // compartilhando as rotas com outros arquivos
